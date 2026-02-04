ec934f89080d96a4550b85d8753852e03b73a530
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-04 16:21:00 +0800

    nested route

---
+++ app/another/nestedanother/page.tsx
@@ -0,0 +1,7 @@
+export default function AnotherNestedPageRoute(){ //it's good practice to use an export default function structure for nested routes
+    return (
+      <div>
+        Another Nested Page Route
+      </div>
+    )
+}
