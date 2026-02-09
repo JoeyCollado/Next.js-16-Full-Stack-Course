0b6b17e10096928e3ec773af181bdf9eed951d71
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-09 14:35:37 +0800

    basic navbar structure

---
+++ navbar/components/web/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export function Navbar() {
+  //components need no default keyword as this is not a full on routes and only a component
+  return (
+    <nav className="flex items-center w-full py-5 justify-between">
+      {" "}
+      {/* navbar container */}
+      {/* Logo */}
+      <div className="flex items-center gap-8">
+        {" "}
+        {/* left */}
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
+        {/* Links */}
+        <div className="flex items-center gap-2">
+          {" "}
+          {/* right */}
+          <Link href="/home">Home</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/create">Create</Link>
+        </div>
+      </div>
+      {/* Button */}
+      <div className="flex items-center gap-2"> {/* further right */}
+        <Link href="/auth/sign-up">Sign up</Link>
+        <Link href="/auth/login">Login</Link>
+      </div> 
+    </nav>
+  );
+}
