25ede2f648d980f0129af4f0c24abd6dc3b6bf35
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-09 14:16:57 +0800

    Accessing blog dynamic page id, using new updated params and destructuring with async and await for new next version

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,7 +1,15 @@
-export default function BlogId(){
+interface BlogIdPageProps{
+    params : Promise<{blogId: string}>;
+}
+
+
+export default async function BlogId({params}: BlogIdPageProps){
+
+    const {blogId} = await params;
     return (
         <div>
             Hello, from dynamic blog page
+            <p>{blogId}</p>
         </div>
     )
 }
