a6a3f7fd3ad04bc026c1ad91cd564cec8f4e88f3
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-10 13:27:54 +0800

    installing zod, react-hook-form, and the react-hook-form resolver

--- navbar/app/auth/sign-up/page.tsx
+++ navbar/app/auth/sign-up/page.tsx
@@ -1,7 +1,21 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function SignUpPage(){
     return (
-        <div>
-            Sign up
-        </div>
+        <Card> {/* container */}
+            <CardHeader> {/* header */}
+                <CardTitle> {/* title */}
+                   Sign up
+                </CardTitle>
+                <CardDescription> {/* description */}
+                   Create an account to get started
+                </CardDescription>
+            </CardHeader>
+            <CardContent> {/* content */}
+               <form>
+                
+               </form>
+            </CardContent> 
+        </Card>
     )
 }
