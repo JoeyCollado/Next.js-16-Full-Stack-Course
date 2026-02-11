c7c8fd3e21893021a5a03730215e22117a91aa4a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-11 16:10:25 +0800

    package conflict resolved

--- navbar/app/auth/sign-up/page.tsx
+++ navbar/app/auth/sign-up/page.tsx
@@ -1,6 +1,19 @@
+import { signUpSchema } from "@/app/schemas/auth";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function SignUpPage(){
+
+    const form = useForm({ //setting up react hook form
+      resolver: zodResolver(signUpSchema), //this will validate our data against the zod schema
+      defaultValues: {
+        name: "",
+        email: "",
+        password: "",
+      }, 
+    });  
+
     return (
         <Card> {/* container */}
             <CardHeader> {/* header */}
--- navbar/app/schemas/auth.ts
+++ navbar/app/schemas/auth.ts
@@ -1,6 +1,7 @@
-import z from 'zod';
+import {z} from 'zod';
+
 export const signUpSchema = z.object({ //setting up schema with zod validation
     name : z.string().min(3).max(30),
     email : z.email(),
-    paasword: z.string().min(8).max(30),
+    password: z.string().min(8).max(30),
 });
