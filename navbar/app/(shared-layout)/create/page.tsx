e7bc322b88609115676a3945e6ede75261ec4932
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-25 18:48:29 +0800

    blog schema for post

--- navbar/app/(shared-layout)/create/page.tsx
+++ navbar/app/(shared-layout)/create/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const CreateRoute = () => {
@@ -5,8 +6,21 @@ const CreateRoute = () => {
     <div className='py-12'>
       <div className='text-center mb-12'> {/* header wrapper */}
         <h1 className='text-4xl font-extrabold tracking-tight sm:text-5xl'>Create Post</h1>
-        <p className='text-xl text-muted-foreground pt-4'>Create your own blog</p>
+        <p className='text-xl text-muted-foreground pt-4'>Start sharing your thoughts</p>
       </div>
+
+      <Card className='w-full max-w-xl mx-auto'>
+        <CardHeader>
+            <CardTitle>Create Blog Article</CardTitle>
+            <CardDescription>Create a new blog article</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+            <form>
+                
+            </form>
+        </CardContent>
+      </Card>
     </div>
   )
 }
