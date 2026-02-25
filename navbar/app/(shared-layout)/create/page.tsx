d5f29659613069dc8d25adea99eff657a23c05ef
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-25 18:43:05 +0800

    initial ui for create route

---
+++ navbar/app/(shared-layout)/create/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CreateRoute = () => {
+  return (
+    <div className='py-12'>
+      <div className='text-center mb-12'> {/* header wrapper */}
+        <h1 className='text-4xl font-extrabold tracking-tight sm:text-5xl'>Create Post</h1>
+        <p className='text-xl text-muted-foreground pt-4'>Create your own blog</p>
+      </div>
+    </div>
+  )
+}
+
+export default CreateRoute
