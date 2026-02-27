104a98ae91067a592e5c556fddcaba3a4e243ef2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-02-27 16:50:34 +0800

    Defining database schema with convex

--- navbar/convex/task.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "./_generated/server";
-
-//api
-export const get = query({ //define query
-  args: {}, //pass arguments
-  handler: async (ctx) => { //define logic
-    return await ctx.db.query("tasks").collect(); //access the db, get data from all the task table and collect it
-  },
-});
