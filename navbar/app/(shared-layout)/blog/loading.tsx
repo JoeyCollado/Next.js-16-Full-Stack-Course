001adc37e032a4d2dbe5b64d97b1362d9605087d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-03-18 13:47:13 +0800

    Streaming using suspense boundaries for static instant rendering

--- navbar/app/(shared-layout)/blog/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function LoadingBlog(){
-    return (
-        <h1 className="text-5xl font-bold text-center">Loading...</h1>
-    )
-}
--- navbar/app/(shared-layout)/blog/page.tsx
+++ navbar/app/(shared-layout)/blog/page.tsx
@@ -4,12 +4,10 @@ import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 
-const BlogPage = async () => {
-
-  //fetching data on server side
-  const data = await fetchQuery(api.posts.getPosts) //by using this we will lose all reactivity
+const BlogPage = () => {
+ 
   return (
     <div>
       <div className="text-center pb-12">
@@ -21,43 +19,55 @@ const BlogPage = async () => {
         </p>
       </div>
 
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((post) => (
-          <Card key={post._id} className="pt-0">
-            <div className="relative h-48 w-full overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1773176647951-d8f618dee942?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                fill
-                alt="blog image"
-                className="rounded-t-lg"
-              />{" "}
-              {/* it's mandatory to put relative in parent component classname for the fill property of image to work as intended */}
-            </div>
-
-            <CardContent>
-              <Link href={`/blog${post._id}`}>
-                <h1 className="text-2xl font-bold hover:text-primary">
-                  {post.title}
-                </h1>
-              </Link>
-              <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-            </CardContent>
-
-            <CardFooter>
-              <Link
-                className={buttonVariants({
-                  className: "w-full",
-                })}
-                href={`/blog${post._id}`}
-              >
-                Read more
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+<Suspense fallback={<p className="text-5xl text-center font-bold">Loading...</p>}>
+      <LoadBlogList/>     
+      </Suspense>
     </div>
   );
 };
 
 export default BlogPage;
+
+async function LoadBlogList(){
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  //fetching data on server side
+  const data = await fetchQuery(api.posts.getPosts) //by using this we will lose all reactivity
+  
+  return(
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    {data?.map((post) => (
+      <Card key={post._id} className="pt-0">
+        <div className="relative h-48 w-full overflow-hidden">
+          <Image
+            src="https://images.unsplash.com/photo-1773176647951-d8f618dee942?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            fill
+            alt="blog image"
+            className="rounded-t-lg"
+          />{" "}
+          {/* it's mandatory to put relative in parent component classname for the fill property of image to work as intended */}
+        </div>
+
+        <CardContent>
+          <Link href={`/blog${post._id}`}>
+            <h1 className="text-2xl font-bold hover:text-primary">
+              {post.title}
+            </h1>
+          </Link>
+          <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+        </CardContent>
+
+        <CardFooter>
+          <Link
+            className={buttonVariants({
+              className: "w-full",
+            })}
+            href={`/blog${post._id}`}
+          >
+            Read more
+          </Link>
+        </CardFooter>
+      </Card>
+    ))}
+  </div>
+  )
+}
