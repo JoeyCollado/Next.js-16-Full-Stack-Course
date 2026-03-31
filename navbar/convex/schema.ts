cf99c987384a57b1094bcc93e30dc33a71986467
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-03-31 21:33:43 +0800

    defining new table in convex schema

--- navbar/convex/schema.ts
+++ navbar/convex/schema.ts
@@ -2,10 +2,16 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    posts: defineTable({
-        title: v.string(),
+    posts: defineTable({  //table 1
+        title: v.string(), //fields
         body: v.string(),
         authorId: v.string(),
         imageStorageId: v.optional(v.id("_storage")),
     }),
+    comments: defineTable({ //table 2
+        postId: v.id("posts"),
+        authorId: v.string(),
+        authorName: v.string(),
+        body: v.string(),
+    })
 });
