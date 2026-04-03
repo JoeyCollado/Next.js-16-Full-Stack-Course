6c3fbab2b3ade51138907760a72e193af0db204e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-04-03 22:01:11 +0800

    Accessing Params on the Client Side - 1 - comment mutation post query

--- navbar/components/web/CommentSection.tsx
+++ navbar/components/web/CommentSection.tsx
@@ -1,33 +1,53 @@
 "use client";
-import React from 'react'
-import { Card, CardContent, CardHeader } from '../ui/card'
-import { MessageSquare } from 'lucide-react'
-import { Controller, useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { commentSchema } from '@/app/schemas/comment'
-import { Field, FieldError, FieldLabel } from '../ui/field'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
+import React, { useTransition } from "react";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Loader2, MessageSquare } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { commentSchema } from "@/app/schemas/comment";
+import { Field, FieldError, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import z from "zod";
+import { toast } from "sonner";
 
 const CommentSection = () => {
+  const [isPending, startTransition] = useTransition();
+  const params = useParams<{ postId: Id<"posts"> }>();
+  const createComment = useMutation(api.comments.createComment);
   const form = useForm({
     resolver: zodResolver(commentSchema),
     defaultValues: {
-        body: "",
-        postId: "",
+      body: "",
+      postId: params.postId,
     },
   });
+
+  async function onSubmit(data: z.infer<typeof commentSchema>) {
+    startTransition(async () => {
+      try {
+        await createComment(data);
+        toast.success("Comment posted successfully");
+      } catch {
+        toast.error("Failed to create post");
+      }
+    })
+  }
 
   return (
     <div>
       <Card>
-        <CardHeader className='flex flex-row items-center gap-2 border-b'>
-            <MessageSquare className='size-5'/>
-            <h2 className='text-xl font-bold'>5 Comments</h2>
+        <CardHeader className="flex flex-row items-center gap-2 border-b">
+          <MessageSquare className="size-5" />
+          <h2 className="text-xl font-bold">5 Comments</h2>
         </CardHeader>
 
         <CardContent>
-            <form className='space-y-4'>
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
             <Controller
               name="body"
               control={form.control}
@@ -42,12 +62,21 @@ const CommentSection = () => {
               )}
             />
 
-            <Button>Submit</Button>
-            </form>
+            <Button disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Loader2 className="size-4 animate-spin" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                <span>Signup</span>
+              )}
+            </Button>
+          </form>
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default CommentSection
+export default CommentSection;
