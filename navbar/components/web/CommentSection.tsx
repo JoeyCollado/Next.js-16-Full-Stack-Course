28db6dbd6c0583f7f5bef9f73ce31a92950d3c56
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-04-04 22:31:13 +0800

    rendering ui for comments fetched data - realtime comment functionality

--- navbar/components/web/CommentSection.tsx
+++ navbar/components/web/CommentSection.tsx
@@ -14,6 +14,8 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import z from "zod";
 import { toast } from "sonner";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Separator } from "../ui/separator";
 
 const CommentSection = () => {
   const params = useParams<{ postId: Id<"posts"> }>();
@@ -40,6 +42,12 @@ const CommentSection = () => {
     })
   }
 
+  if(data === undefined){ //if data is undefined render this
+    return (
+      <p>Loading...</p>
+    )
+  }
+
   return (
     <div>
       <Card>
@@ -48,7 +56,7 @@ const CommentSection = () => {
           <h2 className="text-xl font-bold">5 Comments</h2>
         </CardHeader>
 
-        <CardContent>
+        <CardContent className="space-y-8">
           <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
             <Controller
               name="body"
@@ -76,6 +84,29 @@ const CommentSection = () => {
             </Button>
           </form>
 
+          {data?.length > 0 && <Separator/>} {/* if data length is greater than 0 render separator */}
+
+          <section className="space-y-6">
+            {data?.map((comment) => (
+              <div key={comment._id} className="flex gap-4">
+                <Avatar className="size-10 shrink-0">
+                <AvatarImage src={`https://avatar.vercel.sh/${comment.authorName}`} alt={comment.authorName}/>
+                <AvatarFallback>
+                  {comment.authorName.slice(0,2).toUpperCase()}
+                  </AvatarFallback>
+                  </Avatar>
+                  <div className="flex-1 space-y-1">
+                    <div className="flex items-center justify-between">
+                      <p className="font-semibold text-sm">{comment.authorName}</p>
+                      <p className="text-muted-foreground text-xs">{new Date(comment._creationTime).toLocaleDateString("en-US")}</p>
+                    </div>
+
+                    <p className="text-sm text-foreground/90 whitespace-pre-wrap leading-relaxed">{comment.body}</p>
+                  </div>
+              </div>
+            ))}
+
+          </section>
         </CardContent>
       </Card>
     </div>
