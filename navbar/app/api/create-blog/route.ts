4f548eb321963b3be7964082b123fff7c2e0d653
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-05-07 22:36:08 +0800

    Done

--- navbar/app/api/create-blog/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-//api endpoint example using api routes
-export async function POST(){
-    //logic here
-    console.log("hello from the server");
-
-    return NextResponse.json({success: true});
-}
